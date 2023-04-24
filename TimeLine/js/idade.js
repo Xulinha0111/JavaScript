657a71d0c3edb887a52fc7cb10699c21c17a3e8b
Julia B. Sorrente <107429762+Xulinha0111@users.noreply.github.com>
2023-04-24 14:03:01 -0300

    Delete TimeLine directory

--- TimeLine/js/idade.js
+++
@@ -1,8 +0,0 @@
-function carregaJovem() {
-    let nome = document.getElementById('nome').value;
-    let idade = document.getElementById('idade').value;
-
-    let idadeFutura = parseInt(idade) + 40;
-
-    return document.getElementById('idadeFutura').value = idadeFutura;
-}
